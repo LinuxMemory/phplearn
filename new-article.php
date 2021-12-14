eb525db7aabeb76f9f8312890c6877bb685ea0e3
linuxmemorywork <linuxmemorywork@gmail.com>
2021-12-14 21:17:17 +0100

    new-article.php form beginning

---
+++ new-article.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+	<h1>My blog</h1>
+	
+	<h2>New article</h2>
+	
+	<form method="post">
+		
+		<div>
+			<label for="title">Title</label>
+			<input type="text" name="title" id="title">
+		</div>
+		<br>
+		<div>
+			<label for="content">Content</label>
+			<textarea name="content" id="content"></textarea>
+		</div>
+		<br>
+		<div>
+			<label for="published_time">Published at</label>
+			<input type="date-local" name="published_time" id="published_time">
+		</div>
+		<br>
+		<button>Send</button>
+		
+	</form>
+	
+	
+</body>
+</html>
--- querystring.php
+++ querystring.php
@@ -1,56 +1,31 @@
-<?php 
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
-var_dump($_POST);
-}
-
-
-?>
-
-
-<!DOCTYPE html>
 <html>
-	
-<body>
-	
-	<form method="post">
-		
-		<div>
-			<label for="text">Text:</label>
-			<input type="text" name="text" id="text">
-		</div>
-		
-		<div>
-			<input type="checkbox" name="checkbox" value="yes" id="checkbox">
-			<label for="checkbox">Yes</label>
-		</div>
-		
-		
-		<p>Colours:</p>
-		<div>
-			 <input type="radio" id="blue" name="color" value="blue">
-			 <label for="blue">Blue</label>
-		</div>
-		
-		<div>
-			<input type="radio" id="red" name="color" value="red">
-			<label for="red">Red</label>
-		</div>
-		
-		<div>
-			 <input type="radio" id="green" name="color" value="green">
-			 <label for="green">Green</label>
-		</div>
-	
 
+<body>	
+	<form method="post">	
+		<h4>Form</h4>
 		
-		<button>Send</button>
-		
-	</form>
+		<div>
+			<label for="text">Name</label>
+			<input type="text" id="text" name="text">
+		</div>
 
+		<div>
+			<label for="textarea">Description</label>
+			<textarea id="textarea" name="textarea"></textarea>
+		</div>
+  
+		<div>
+			<label for="select">Size</label>
+			<select name="select" id="select">
+				<option>One</option>
+				<option>Two</option>
+				<option>Three</option>
+			</select>
+		</div>
+  </form>
+
+    
+     
 </body>
-	
 </html>
-	
-	
+  
